5b9f2d8089553b85af040cce747fcb4b65af569d
Regiber18 <regiber.montoya2005@hotmail.com>
2024-09-02 16:09:35 -0600

    commit adding fetch

--- src/assets/components/organims/CharactersSection.jsx
+++ src/assets/components/organims/CharactersSection.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import Card from "../molecules/Card";
 import './Characters.css'
+import { useState } from "react";
 function CharactersSection() {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch("https://dragonball-api.com/api/characters")
+        .then((reponse) => {
+            if(reponse.ok) {
+                return reponse.json()
+            }
+        })
+        .then((data) => {
+            setData(data)
+        })
+        .then(() =>  {
+            console.log(data)
+        })
+    })
     return(
         <div className="container">
-           <Card></Card> 
+           <Card data={data}></Card> 
         </div>
     )
 }
